ea9fd3601aeae3e138e9aabbb0770749a7ac80c6
Muhammedshifin <muhammedshifink4@gmail.com>
2024-05-07 19:50:41 +0530

    Create join_reqs.py

---
+++ database/join_reqs.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+import motor.motor_asyncio
+from info import REQ_CHANNEL, REQ_CHANNEL2
+
+class JoinReqs:
+
+    def __init__(self):
+        from info import JOIN_REQS_DB
+        if JOIN_REQS_DB:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(JOIN_REQS_DB)
+            self.db = self.client["JoinReqs"]
+            self.col = self.db[str(REQ_CHANNEL)]
+            self.chat_col = self.db["ChatId"]
+        else:
+            self.client = None
+            self.db = None
+            self.col = None
+
+    def isActive(self):
+        if self.client is not None:
+            return True
+        else:
+            return False
+
+    async def add_user(self, user_id, first_name, username, date):
+        try:
+            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
+        except:
+            pass
+
+    async def get_user(self, user_id):
+        return await self.col.find_one({"user_id": int(user_id)})
+
+    async def get_all_users(self):
+        return await self.col.find().to_list(None)
+
+    async def delete_user(self, user_id):
+        await self.col.delete_one({"user_id": int(user_id)})
+
+    async def delete_all_users(self):
+        await self.col.delete_many({})
+
+    async def get_all_users_count(self):
+        return await self.col.count_documents({})
+        
+    async def add_fsub_chat(self, chat_id):
+        try:
+            await self.chat_col.delete_many({})
+            await self.chat_col.insert_one({"chat_id": chat_id})
+        except:
+            pass
+
+    async def get_fsub_chat(self):
+        return await self.chat_col.find_one({})
+
+    async def delete_fsub_chat(self, chat_id):
+        await self.chat_col.delete_one({"chat_id": chat_id})
+
+
+class JoinReqs2:
+
+    def __init__(self):
+        from info import JOIN_REQS_DB
+        if JOIN_REQS_DB:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(JOIN_REQS_DB)
+            self.db2 = self.client["JoinReqs2"]
+            self.col = self.db2[str(REQ_CHANNEL2)]
+            self.chat_col = self.db2["ChatId2"]
+        else:
+            self.client = None
+            self.db = None
+            self.col = None
+
+    def isActive(self):
+        if self.client is not None:
+            return True
+        else:
+            return False
+
+    async def add_user(self, user_id, first_name, username, date):
+        try:
+            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
+        except Exception as e:
+            print(e)
+
+    async def get_user(self, user_id):
+        return await self.col.find_one({"user_id": int(user_id)})
+
+    async def get_all_users(self):
+        return await self.col.find().to_list(None)
+
+    async def delete_user(self, user_id):
+        await self.col.delete_one({"user_id": int(user_id)})
+
+    async def delete_all_users(self):
+        await self.col.delete_many({})
+
+    async def get_all_users_count(self):
+        return await self.col.count_documents({})
+        
+    async def add_fsub_chat(self, chat_id):
+        try:
+            await self.chat_col.delete_many({})
+            await self.chat_col.insert_one({"chat_id": chat_id})
+        except:
+            pass
+
+    async def get_fsub_chat(self):
+        return await self.chat_col.find_one({})
+
+    async def delete_fsub_chat(self, chat_id):
+        await self.chat_col.delete_one({"chat_id": chat_id})
