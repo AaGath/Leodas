309f5bf0afbda8f218bdbbb2a4882948dae5e139
Muhammedshifin <muhammedshifink4@gmail.com>
2024-05-08 12:32:49 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -4,7 +4,7 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from database.join_reqs import JoinReqs
-from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS, FSUB_MODE
+from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
 
 from logging import getLogger
 
