09009840a143772b3f5574734575bf9b14f6f633
AaGath <89765070+AaGath@users.noreply.github.com>
2024-05-08 23:34:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ
 ✯ Mʏ Vᴇʀꜱɪᴏɴ: ꜱᴀᴅɪᴇ-ʙᴏᴛ ᴠ4.5.0</b>"""
    
     SOURCE_TXT = """<b>NOTE:</b>"""
-- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› : <a href='https://t.me/Wireless_TG'>ꜱᴀᴅɪᴇ ꜱɪɴᴋ ʙᴏᴛ</a>
+"""<b>- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› : <a href='https://t.me/Wireless_TG'>ꜱᴀᴅɪᴇ ꜱɪɴᴋ ʙᴏᴛ</a></b>"""
 
 """<b>ᴅᴇᴠ: <a herf='https://t.me/Wireless_TG'>ᴡɪʀᴇʟᴇss 🇮🇳 I 🇮🇹</a> </b>"""
 
