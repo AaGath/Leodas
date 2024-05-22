359952e6d2822c343f3bb22d3c8e29499622a445
AaGath <89765070+AaGath@users.noreply.github.com>
2024-05-22 12:42:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,16 +8,16 @@ I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ
     HELP_TXT = "Hᴇʏ {}\nHᴇʀᴇ Mꜱ Mʏ Hᴇʟᴩ"
 
     ABOUT_TXT = """<b>✯ Mʏ ɴᴀᴍᴇ: {}
-✯ Dᴇᴠᴇʟᴏᴩᴇʀ:<a href='https://t.me/ame0451'>ame</a> 
+✯ Dᴇᴠᴇʟᴏᴩᴇʀ:<a href='https://t.me/ckadmin_bot'>CK ADMIN</a> 
 ✯ Cᴏᴅᴇᴅ Oɴ: ᴩʏᴛʜᴏɴ/ᴩʏʀᴏɢʀᴀᴍ
 ✯ Mʏ DᴀᴛᴀBᴀꜱᴇ: ᴍᴏɴɢᴏ-ᴅʙ
 ✯ Mʏ Sᴇʀᴠᴇʀ: ᴀɴʏᴡʜᴇʀᴇ
 ✯ Mʏ Vᴇʀꜱɪᴏɴ: LEO DAS ᴠ4.5.0</b>"""
    
     SOURCE_TXT = """<b>NOTE:</b>
-- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href='https://t.me/ame0451'>ꜱᴀᴅɪᴇ ꜱɪɴᴋ ʙᴏᴛ</a>
+- ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʜᴇʀᴇ ◉› :<a href='https://t.me/ckadmin_bot'>LEO DAS ʙᴏᴛ</a>
 
-<b>ᴅᴇᴠ: <a href='https://t.me/ame0451'>ame</a> </b>"""
+<b>ᴅᴇᴠ: <a href='https://t.me/ckadmin_bot'>CK ADMIN</a> </b>"""
 
     FILE_TXT = """<b>➤ Hᴇʟᴘ Fᴏʀ Fɪʟᴇ Sᴛᴏʀᴇ</b>
 
